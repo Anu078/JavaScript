209b834e26fd3c43b861294e978f33837862b738
Mohammad Anwar Ahmed <mohdanwarahmed68@gmail.com>
2024-09-27 10:31:39 +0000

    container config done

---
+++ 01_Basic.js/variable.js
@@ -0,0 +1,17 @@
+const accountId = 078;
+let accountEmail = "mohdanwarahmed68@gmail.com";
+var accountPassword = "525252";
+accountCity = "Hyderabad"
+
+// accountId = 086 we cannot change const value once declare
+
+accountEmail = "hashmi9301@gmail.com";
+accountPassword = "212121";
+accountCity = "Mumbai";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
